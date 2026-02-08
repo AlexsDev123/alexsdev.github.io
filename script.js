a72f332af17e5e2837f00eb994fc6d0770856535
AlexsDev123 <zhackedcontacto@gmail.com>
2026-02-07 22:42:52 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function saludar() {
-  alert("Funciona GitHub Pages 😎");
+function updateClock() {
+  const now = new Date();
+
+  const h = String(now.getHours()).padStart(2, '0');
+  const m = String(now.getMinutes()).padStart(2, '0');
+  const s = String(now.getSeconds()).padStart(2, '0');
+
+  document.getElementById('clock').textContent = `${h}:${m}:${s}`;
+
+  const d = String(now.getDate()).padStart(2, '0');
+  const mo = String(now.getMonth() + 1).padStart(2, '0');
+  const y = now.getFullYear();
+
+  document.getElementById('date').textContent = `${d}/${mo}/${y}`;
 }
+
+setInterval(updateClock, 1000);
+updateClock();
